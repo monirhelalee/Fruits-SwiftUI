42cccb316852014f8b65dc1d5121b827baf05285
monir <mdmonirhelale@gmail.com>
2024-05-23 00:32:38 +0600

    Initial Commit

---
+++ Fruits-SwiftUI/Fruits_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_SwiftUIApp.swift
+//  Fruits-SwiftUI
+//
+//  Created by Monir Haider Helalee on 23/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
